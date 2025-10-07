b980fdb8b9f0f99b5e2cb2f129a69cecc2e9e802
Muhammad Ahtasham <ahtashammuzamal@gmail.com>
2025-10-07 21:10:30 +0500

    practice files removed

--- src/utils/password-hashing.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-const hashPassword = async (password) => {
-  const hashedpassword = await bcrypt.hash(password, 8);
-  console.log("Hashed password: ", hashedpassword);
-
-  const isMatch = await bcrypt.compare(password, hashedpassword);
-  return isMatch;
-};
